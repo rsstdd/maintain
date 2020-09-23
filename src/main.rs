6f4e1b1ba4c65a63a3a93ae65e19d74515c7a69e
Ross Todd <rosstodd@lululemon.com>
2020-09-22 22:56:31 -0700

    format

--- src/main.rs
+++ src/main.rs
@@ -6,45 +6,45 @@ use indicatif::{HumanDuration, ProgressBar, ProgressStyle};
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 fn to_words<'a>(text: &'a str) -> impl Iterator<Item = &'a str> {
-  text.split(' ')
+    text.split(' ')
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-  let started = Instant::now();
-  let brew_commands: [&str; 7] = [
-    "update",
-    "upgrade",
-    "cleanup",
-    "cask cleanup",
-    "doctor",
-    "missing",
-    "prune",
-  ];
+    let started = Instant::now();
+    let brew_commands: [&str; 7] = [
+        "update",
+        "upgrade",
+        "cleanup",
+        "cask cleanup",
+        "doctor",
+        "missing",
+        "prune",
+    ];
 
-  let bar = ProgressBar::new(8);
-  bar.set_style(ProgressStyle::default_bar().template(
-      "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] ({pos}/{len}, ETA {eta})",
-  ));
+    let bar = ProgressBar::new(8);
+    bar.set_style(ProgressStyle::default_bar().template(
+            "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] ({pos}/{len}, ETA {eta})",
+    ));
 
-  let mut stdout = StandardStream::stdout(ColorChoice::Always);
-  stdout.set_color(ColorSpec::new().set_fg(Some(Color::Rgb(253, 117, 50))))?;
-  print!("Executing Brew ");
-  for command in bar.wrap_iter(brew_commands.iter()) {
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
     stdout.set_color(ColorSpec::new().set_fg(Some(Color::Rgb(253, 117, 50))))?;
-    println!("{:?}", command);
-    let output = Command::new("Brew")
-      .args(to_words(command))
-      .stderr(Stdio::null())
-      .output()
-      .expect("Brew command failed");
+    print!("Executing Brew ");
+    for command in bar.wrap_iter(brew_commands.iter()) {
+        stdout.set_color(ColorSpec::new().set_fg(Some(Color::Rgb(253, 117, 50))))?;
+        println!("{:?}", command);
+        let output = Command::new("Brew")
+            .args(to_words(command))
+            .stderr(Stdio::null())
+            .output()
+            .expect("Brew command failed");
 
-      stdout.set_color(ColorSpec::new().set_fg(Some(Color::Rgb(30, 151, 70))))?;
-      io::stdout().write_all(&output.stdout).unwrap();
-      stdout.set_color(ColorSpec::new().set_fg(Some(Color::Rgb(207, 0, 15))))?;
-      io::stderr().write_all(&output.stderr).unwrap();
-  }
-  bar.finish_and_clear();
-  stdout.set_color(ColorSpec::new().set_fg(Some(Color::Rgb(99, 192, 223))))?;
-  println!("Done in {}", HumanDuration(started.elapsed()));
-  Ok(())
+        stdout.set_color(ColorSpec::new().set_fg(Some(Color::Rgb(30, 151, 70))))?;
+        io::stdout().write_all(&output.stdout).unwrap();
+        stdout.set_color(ColorSpec::new().set_fg(Some(Color::Rgb(207, 0, 15))))?;
+        io::stderr().write_all(&output.stderr).unwrap();
+    }
+    bar.finish_and_clear();
+    stdout.set_color(ColorSpec::new().set_fg(Some(Color::Rgb(99, 192, 223))))?;
+    println!("Done in {}", HumanDuration(started.elapsed()));
+    Ok(())
 }
