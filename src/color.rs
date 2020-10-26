042560596c95fdabcd3678cdc3e190d9b8932b07
Ross Todd <rtodd@lululemon.com>
2020-10-25 21:44:52 -0700

    Revert to barebones

---
+++ src/color.rs
@@ -0,0 +1,19 @@
+use std::io::{self, Write};
+use std::error::Error;
+use std::str::FromStr;
+
+// pub static COLOR_STATUS: &Color = &Color::Term(TermColor::Green);
+// pub static COLOR_ERROR: &Color = &Color::Term(TermColor::Red);
+// pub static COLOR_INFO: &Color = &Color::Term(TermColor::Cyan);
+// pub static COLOR_WARN: &Color = &Color::Term(TermColor::Yellow);
+// pub static COLOR_VERBOSE: &Color = &Color::Term(TermColor::Blue);
+
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
+
+mod color {
+  pub fn write_color(color: <SOME TYPE OF COLOR> ) -> io::Result<()> {
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+    stdout.set_color(ColorSpec::new().set_fg(Some(Color::color)))?;
+    io::stdout().write_all(&output.stdout).unwrap();
+  }
+}
